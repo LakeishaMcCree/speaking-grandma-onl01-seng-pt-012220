8a98fe0821c6f34c386aee52a09ad0a445b2a30d
Lakeisha McCree <lmccree215@icloud.com>
2020-01-30 01:48:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase 
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase == "hi!"
+  else phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
